7d7c7366e04ab74f678cac1813153630d7929b55
TithiSreemany <69536486+TithiSreemany@users.noreply.github.com>
2021-11-22 21:48:25 +0530

    Time: 6 ms (69.35%), Space: 115.2 MB (25.50%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.java
+++ palindrome-linked-list/palindrome-linked-list.java
@@ -8,47 +8,39 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-
 class Solution 
 {
-    public boolean isPalindrome(ListNode head) 
-    {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next!=null && fast.next.next!=null)
-        {
-            slow=slow.next;
-            fast = fast.next.next;
-        }
-        if (slow.next == null)
-            return true;
-        slow.next = reverse(slow.next);
-        fast = head;
+    public boolean isPalindrome(ListNode head) {
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
         slow = slow.next;
-        while (slow!=null)
-        {
-            if (slow.val!=fast.val)
-                return false;
-            slow=slow.next;
-            fast = fast.next;
-        }
-        return true;
     }
-    public ListNode reverse(ListNode start)
-    {
-        ListNode temp = new ListNode();
-        ListNode sec = new ListNode();
-        temp=null;
-        sec=null;
-        while (start.next!=null)
-        {
-            sec=start.next;
-            start.next=temp;
-            temp=start;
-            start=sec;
-        }
-        start.next= temp;
-        return start;
+    if (fast != null) { // odd nodes: let right half smaller
+        slow = slow.next;
     }
+    slow = reverse(slow);
+    fast = head;
+    
+    while (slow != null) {
+        if (fast.val != slow.val) {
+            return false;
+        }
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return true;
+}
+
+public ListNode reverse(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+
 }
