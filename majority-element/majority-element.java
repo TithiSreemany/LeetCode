3cdaf713684c7decacce0bed4b3e1e79487aafae
TithiSreemany <69536486+TithiSreemany@users.noreply.github.com>
2021-11-23 00:50:58 +0530

    Time: 1 ms (99.90%), Space: 54.6 MB (5.46%) - LeetHub

--- majority-element/majority-element.java
+++ majority-element/majority-element.java
@@ -5,13 +5,13 @@ class Solution
     // Space Complexity : O(1)
     public int majorityElement(int[] nums) 
     {
-        int count = 0;
-        int ele =0;
-        for (int num : nums)
+        int count =0;
+        int ele = nums[0];
+        for(int i=0;i<nums.length;i++)
         {
             if (count==0)
-                ele=num;
-            if (ele==num)
+                ele = nums[i];
+            if (ele==nums[i])
                 count++;
             else
                 count--;
