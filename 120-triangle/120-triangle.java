cc7d6478a5dcad901a3c34b601e965b78387b41e
TithiSreemany <69536486+TithiSreemany@users.noreply.github.com>
2022-03-13 11:18:42 +0530

    Time: 4 ms (63.83%), Space: 44.1 MB (33.71%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,5 +1,6 @@
 class Solution 
 {
+    /*
     public int minimumTotal(List<List<Integer>> triangle) 
     {
         int s= triangle.size();
@@ -16,5 +17,28 @@ class Solution
             }
         }
         return dp[0][0];
+    }
+    */
+    
+    //Space Optimised Solution
+    public int minimumTotal(List<List<Integer>> triangle) 
+    {
+        int s= triangle.size();
+        int temp[]=new int [s];
+        int curr[]=new int [s];
+        for(int i=0; i<s;i++)
+        {
+            temp[i]=triangle.get(s-1).get(i);
+        }
+        for(int i=s-2; i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                curr[j]=triangle.get(i).get(j)+Math.min(temp[j], temp[j+1]);
+            }
+            for(int k=0; k<s;k++)
+                temp[k]=curr[k];
+        }
+        return temp[0];
     }
 }
